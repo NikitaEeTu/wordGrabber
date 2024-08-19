534aa571f55bd6ae2ef4974e68bc99a26bfe329a
nikitapodkopaev <nikita71140@gmail.com>
2024-08-19 15:00:21 +0300

    *add translation to a word

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,23 +1,42 @@
+import { generateElement, getData, removeAllElements } from "./helper.js";
 
-import { generateElement, removeAllElements } from "./helper.js";
-const delBtn = document.querySelector("#delBtn");
+const delBtn = document.getElementById("delBtn");
+const selectedWordElem = document.getElementById("word");
+const translatedWordElem = document.getElementById("translatedWord");
+const urls = {
+    wordId: "https://ekilex.ee/api/word/search",
+    wordTranslation: "https://ekilex.ee/api/word/details",
+    dict: "eki"
+}
 document.addEventListener("DOMContentLoaded", function() {
     let port = chrome.runtime.connect({name: "hello"});
 
-    port.onMessage.addListener(function(message) {
-        if (message.word) {
+    port.onMessage.addListener(async function (message) {
+        const clickedWord = message.word;
+        if (clickedWord) {
             const elemData = {
                 name: "p",
                 parent: "grabbed-words"
             }
-            document.getElementById("word").innerText = message.word;
-            chrome.storage.sync.get(['words'], function(data) {
+            selectedWordElem.innerText = clickedWord;
+            translatedWordElem.innerText = await getTranslation(urls, clickedWord);
+            chrome.storage.sync.get(["words"], function (data) {
                 const formattedWords = JSON.parse(data.words);
                 generateElement(formattedWords, elemData)
             });
         }
     });
 });
+
+const getTranslation = async (urls, searchWord) => {
+    const wordIdRes = await getData(`${urls.wordId}/${searchWord}`);
+    const wordId = wordIdRes.words[0].wordId;
+    const wordTransRes = await getData(`${urls.wordTranslation}/${wordId}/${urls.dict}`);
+    const varLangWords = wordTransRes.lexemes[0].synonymLangGroups;
+    const rusWords = varLangWords.filter(word => word.lang === "rus");
+    return rusWords[0].synonyms[0].words[0].wordValue
+}
+
 delBtn.addEventListener("click", () => {
     chrome.storage.sync.set({'words': "[]"}, function() {
     });
--- scripts/helper.js
+++ scripts/helper.js
@@ -1,13 +1,34 @@
+import { EKILEX_API_KEY } from "../settings/key";
+
 export const generateElement = (arr, elemData) => arr.forEach(elemText => {
     const htmlElem = document.createElement(elemData.name);
     htmlElem.innerText = elemText;
-    const parentElem = document.querySelector("#" + elemData.parent);
+    const parentElem = document.getElementById(elemData.parent);
     parentElem.appendChild(htmlElem);
 })
 
 export const removeAllElements = (parent) => {
-    const parentElem = document.querySelector("#" + parent);
+    const parentElem = document.getElementById(parent);
     while(parentElem.hasChildNodes()) {
         parentElem.firstChild.remove();
+    }
+}
+
+export const getData = async (url) => {
+    try {
+        const response = await fetch(url,
+            {
+                method: "GET",
+                headers: {
+                   "ekilex-api-key": EKILEX_API_KEY
+                },
+            });
+        if(!response.ok) {
+            throw new Error("Response status: " + response.status);
+        }
+        return await response.json()
+    }
+    catch (error) {
+        console.error(error.message);
     }
 }
